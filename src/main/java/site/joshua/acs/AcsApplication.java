fd5f99a982161875cd07e1fb26d1c13aac2f0969
Jiwon Seok <ayan9414@gmail.com>
2023-05-06 17:49:27 +0900

    refactoring start

--- src/main/java/site/joshua/acs/AcsApplication.java
+++ src/main/java/site/joshua/acs/AcsApplication.java
@@ -1,11 +1,9 @@
 package site.joshua.acs;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = "site.joshua.acs")
 public class AcsApplication {
 
 	public static void main(String[] args) {
