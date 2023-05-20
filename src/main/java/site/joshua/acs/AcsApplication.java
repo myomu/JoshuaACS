f54cdd9aadc9df7f7f50d2a121c1095056a7d94f
Jiwon Seok <ayan9414@gmail.com>
2023-05-20 19:23:43 +0900

    Timezone 문제 테스트

--- src/main/java/site/joshua/acs/AcsApplication.java
+++ src/main/java/site/joshua/acs/AcsApplication.java
@@ -1,11 +1,18 @@
 package site.joshua.acs;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class AcsApplication {
 
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AcsApplication.class, args);
 	}
